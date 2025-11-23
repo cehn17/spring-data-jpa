da1bdfc922aac8ed589ad9e0e2410ba7dabd1274
Cesar Niveyro <cehn17@gmail.com>
2025-11-23 16:03:00 -0300

    ADD: agregando datos a la base de datos

---
+++ src/main/java/com/cehn17/spring/data/jpa/product/infrastructure/database/seeder/ProductSeeder.java
@@ -0,0 +1,39 @@
+package com.cehn17.spring.data.jpa.product.infrastructure.database.seeder;
+
+import com.cehn17.spring.data.jpa.product.infrastructure.database.entity.ProductEntity;
+import com.cehn17.spring.data.jpa.product.infrastructure.database.repository.QueryProductRepository;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class ProductSeeder implements CommandLineRunner {
+
+    private final QueryProductRepository productRepository;
+    private final ResourceLoader resourceLoader;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        long count = productRepository.count();
+
+        if (count == 0) {
+
+            Resource resource = resourceLoader.getResource("classpath:data.json");
+
+            List<ProductEntity> products = objectMapper.readValue(resource.getInputStream(), new TypeReference<>() {
+            });
+
+            productRepository.saveAll(products);
+        }
+
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO products (id, description, image, name, price)
+VALUES (1,'Auriculares inalámbricos con cancelación de ruido y 38h de autonomía.','Ba1-headphones.jpg','Auriculares Bluetooth Pro',129.99),
+       (2,'Teclado mecánico RGB con switches silenciosos y estructura netálica.','0a2-keyboard.jpg','Teclado Mecánico RGB',89.50),
+       (3,'Monitor 27 2K IPS con 165Hz ideal para productividad y gaming.','0a3-monitor.jpg','Monitor 2K 165Hz',279.00),
+       (4,'Micrófono USB con calidad de estudio, compatible con PC y Mac.','Oa4-microfono.jpg','Microfono Studio USB',69.90),
+       (5,'Silla ergonómica con soporte lumbar y diseño transpirable.','8as-silla.jpg','Silla Ergonomica Pro',249.95),
+       (6,'Ratón inalámbrico con sensor óptico de alta precisión.','0a6-mouse.jpg','Raton Optico PRO',39.99),
+       (7,'Base refrigeradora con 5 ventiladores para portátiles hasta 17','0a7-base-refrigerada.jpg','Base Cooler Laptop',24.99),
+       (8,'Hub USB-C con HDMI, lector SD y 3 puertos USB 3.0.','0a8-hub.jpg','Hub USB-C 6 en 1',34.95),
+       (9,'Altavoces estéreo con subwoofer de 30W y conexión Bluetooth','0a9-alatavoces.jpg','Altavoces Bluetooth 30W',59.99),
+       (10,'Webcam FULL HD con enfoque automático y micrófono integrado.','0a10-webcam.jpg','Webcam FULL HD',49.99);
+
