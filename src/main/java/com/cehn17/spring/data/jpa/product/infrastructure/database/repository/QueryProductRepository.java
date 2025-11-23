66018658ab4d0808f0ab5403fe980f6be4010957
Cesar Niveyro <cehn17@gmail.com>
2025-11-23 16:43:53 -0300

    ADD: querys en el QueryProductRepository

--- src/main/java/com/cehn17/spring/data/jpa/product/infrastructure/database/repository/QueryProductRepository.java
+++ src/main/java/com/cehn17/spring/data/jpa/product/infrastructure/database/repository/QueryProductRepository.java
@@ -1,9 +1,37 @@
 package com.cehn17.spring.data.jpa.product.infrastructure.database.repository;
 
 import com.cehn17.spring.data.jpa.product.infrastructure.database.entity.ProductEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
 
 @Repository
 public interface QueryProductRepository extends JpaRepository<ProductEntity, Long> {
+    Optional<ProductEntity> findByNameContaining(String name);
+
+    List<ProductEntity> findAllByPriceBetween(Double priceStart, Double priceEnd);
+
+    @Query(value = """
+            SELECT p
+            FROM ProductEntity p
+            WHERE p.name LIKE CONCAT('%', :name, '%')
+            OR p.description LIKE CONCAT('%', :description, '%')
+            OR p.price BETWEEN :priceAfter AND :priceBefore
+            """
+    )
+    List<ProductEntity> findProductDetails(String name, String description, Double priceAfter, Double priceBefore);
+
+    boolean existsByName(String name);
+
+    long countByPrice(Double price);
+
 }
